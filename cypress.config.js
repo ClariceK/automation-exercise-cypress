00bdf6837cac63ef9aa00a8f9d59c741e90bb7bb
clarice.k.oliveira <clarice.k.oliveira@accenture.com>
2026-05-18 17:30:35 -0300

    Alteração no cypress config para gravar o vídeo da execução

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
 
   env: require('./cypress/cypress.env.json'),
   
-  video: false,
+  video: true,
   videosFolder: "cypress/videos",
   videoCompression: 32,
   trashAssetsBeforeRuns: true,
